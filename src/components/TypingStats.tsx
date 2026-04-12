efb8b18692a9876d81b9cbbfdaac691f3ea1bb98
nomnng <denis.rbn0@gmail.com>
2026-04-12 20:03:27 +0300

    Added miliseconds to typing stats

--- src/components/TypingStats.tsx
+++ src/components/TypingStats.tsx
@@ -9,13 +9,18 @@ interface TypingStatsProps {
 	name: string;
 };
 
+const UPDATES_PER_SECOND = 3;
+
 function TypingStats({logger, typingStatus, stopOnFinish, name}: TypingStatsProps) {
 	const [timeElapsed, setTimeElapsed] = useState(0);
 	const [typedWords, setTypedWords] = useState(0);
 	const [totalWords, setTotalWords] = useState(0);
 
 	const updateStats = () => {
-		if (!stopOnFinish && !logger.hasMoreEntries()) {
+		const isFinished = stopOnFinish ?
+			(typingStatus === TypingStatus.Finished) : (!logger.hasMoreEntries());
+
+		if (isFinished) {
 			setTimeElapsed(logger.getCurrentTimestamp());
 		} else {
 			setTimeElapsed(logger.getTimeSinceStart());
@@ -26,23 +31,22 @@ function TypingStats({logger, typingStatus, stopOnFinish, name}: TypingStatsProp
 	};
 
 	useEffect(() => {
-		if (
-			typingStatus === TypingStatus.Ready ||
-			(stopOnFinish && typingStatus === TypingStatus.Finished)
-		) {
+		if (typingStatus === TypingStatus.Ready) {
+			return;
+		} else if (stopOnFinish && typingStatus === TypingStatus.Finished) {
+			updateStats();
 			return;
 		}
 
 		let interval: ReturnType<typeof setInterval> = setInterval(() => {
-			updateStats();
 			if (!stopOnFinish && !logger.hasMoreEntries()) {
 				clearInterval(interval);
 			}
-		}, 500);
+			updateStats();
+		}, 1000 / UPDATES_PER_SECOND);
 
 		return () => {
 			clearInterval(interval);
-			updateStats();
 		};
 	}, [typingStatus]);
 
@@ -50,11 +54,21 @@ function TypingStats({logger, typingStatus, stopOnFinish, name}: TypingStatsProp
 	const wpm = minutesSinceStart ? (typedWords / minutesSinceStart) : 0;
 	const completion = totalWords ? (typedWords / totalWords) : 0;
 
-	const formatTime = (ms: number) => {
-		const seconds = ms / 1000;
-		const mStr = Math.floor(seconds / 60).toString().padStart(2, '0');
-		const sStr = Math.floor(seconds % 60).toString().padStart(2, '0');
-		return [mStr, sStr].join(":");
+	const formatTime = (time: number, includeMs: boolean) => {
+		const minutes = Math.floor(time / 60000);
+		const seconds = Math.floor((time % 60000) / 1000);
+		const milliseconds = Math.floor((time % 1000) / 10);
+
+		const mStr = minutes.toString().padStart(2, '0');
+		const sStr = seconds.toString().padStart(2, '0');
+
+		let finalStr = `${mStr}:${sStr}`;
+		if (includeMs) {
+			const msStr = milliseconds.toString().padStart(2, '0');
+			finalStr += `.${msStr}`;
+		}
+
+		return finalStr;
 	};
 
 	const completionTextColor = completion === 1 ? "text-green-500" : "";
@@ -62,7 +76,7 @@ function TypingStats({logger, typingStatus, stopOnFinish, name}: TypingStatsProp
 	return (
 		<div className="grid grid-cols-4 w-full">
 			<div className=	"justify-self-start">{name}</div>
-			<div className="justify-self-start">{formatTime(timeElapsed)}</div>
+			<div className="justify-self-start">{formatTime(timeElapsed, completion === 1)}</div>
 			<div className={"justify-self-center " + completionTextColor}>{(completion * 100).toFixed(2)}%</div>
 			<div className="justify-self-end">{Math.floor(wpm)} WPM</div>
 		</div>
